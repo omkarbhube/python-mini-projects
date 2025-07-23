ce61e5d7a912869a81b9422f87571199fdfe312d
Omkar Bhube <omkarbhube44@gmail.com>
2025-07-23 23:44:44 +0530

    Rock Paper Scissor

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,6 +1,8 @@
 import random
 user_choice = int(input("Enter 0 for Rock  1 for Paper  2 for Scissor: "))
 comp_choice = random.randint(0,2)
+print(f"You Entered: {user_choice}")
+print(f"Computer Entered: {comp_choice}")
 # 0 - Draw, 1 - Win, 2 - Lose
 possibilities = [
                  [0,2,1],
@@ -10,7 +12,7 @@ possibilities = [
 if possibilities[user_choice] [comp_choice] == 0:
     print("Draw")
 elif possibilities[user_choice] [comp_choice] == 1:
-    print("You Wins!")
+    print("You Win!")
 elif possibilities[user_choice] [comp_choice] == 2:
     print("You Lose!")
 else:
